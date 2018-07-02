f442b7193de9a72c9449f5385e141de8e8108262
Mocha.16 <Mocha.16@kakaocorp.com>
2018-07-03 00:12:49 +0900

    잘잘한거 지움

--- src/shuffling.jsx
+++ src/shuffling.jsx
@@ -7,7 +7,6 @@ export default class Shuffle extends React.Component{
         return(
             <div className="tableset">
             <img src="https://d258y123k6w0j.cloudfront.net/goods//1522050158638l0[1].jpg" className="uniformcard" />
-                Hi I'm in
             </div>
         )
     }
