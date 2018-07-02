32e6f228f6aa97e913811f41f6cfdb06ae33a967
Mocha.16 <Mocha.16@kakaocorp.com>
2018-07-02 23:46:32 +0900

    단순 애니메이션 구현

---
+++ src/shuffling.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './shuffle.css'
+
+export default class Shuffle extends React.Component{
+
+    render(){
+        return(
+            <div className="tableset">
+            <img src="https://d258y123k6w0j.cloudfront.net/goods//1522050158638l0[1].jpg" className="uniformcard" />
+                Hi I'm in
+            </div>
+        )
+    }
+
+
+}
