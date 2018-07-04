796bc02bdfed4bd4953d592f0d006c83612be6d6
Mocha.16 <Mocha.16@kakaocorp.com>
2018-07-04 23:29:21 +0900

    [#1] react-animated-slider 첫 부착

--- src/shuffling.jsx
+++ src/shuffling.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
 import './shuffle.css'
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
 
-export default class Shuffle extends React.Component{
+export default class Shuffle extends React.Component {
 
-    render(){
-        return(
-            <div className="tableset">
-            <img src="https://d258y123k6w0j.cloudfront.net/goods//1522050158638l0[1].jpg" className="uniformcard" />
-            </div>
+    render() {
+        let index = 0
+        return (
+            // <div className="uniformcard">
+            //     <img src="https://d258y123k6w0j.cloudfront.net/goods//1522050158638l0[1].jpg" className="uniformcard2" />
+            //     <img src="https://d258y123k6w0j.cloudfront.net/goods//1522050158638l0[1].jpg" className="uniformcard2" />
+            //     <img src="https://d258y123k6w0j.cloudfront.net/goods//1522050158638l0[1].jpg" className="uniformcard2" />
+            //     <img src="https://d258y123k6w0j.cloudfront.net/goods//1522050158638l0[1].jpg" className="uniformcard2" />
+            //     <img src="https://d258y123k6w0j.cloudfront.net/goods//1522050158638l0[1].jpg" className="uniformcard2" />
+            //     <img src="https://d258y123k6w0j.cloudfront.net/goods//1522050158638l0[1].jpg" className="uniformcard2" />
+            //     <img src="https://d258y123k6w0j.cloudfront.net/goods//1522050158638l0[1].jpg" className="uniformcard2" />
+            //     <img src="https://d258y123k6w0j.cloudfront.net/goods//1522050158638l0[1].jpg" className="uniformcard2" />
+            //     <img src="https://d258y123k6w0j.cloudfront.net/goods//1522050158638l0[1].jpg" className="uniformcard2" />
+            // </div>
+            <Slider className="slider-wrapper">
+                <div key={index++} className="slider-content">
+                    <img src="https://d258y123k6w0j.cloudfront.net/goods//1522050158638l0[1].jpg" className="uniformcard2" />
+                </div>
+                <div key={index++} className="slider-content">
+                    <img src="https://d258y123k6w0j.cloudfront.net/goods//1522050158638l0[1].jpg" className="uniformcard2" />
+                </div>
+                <div key={index++} className="slider-content">
+                    <img src="https://d258y123k6w0j.cloudfront.net/goods//1522050158638l0[1].jpg" className="uniformcard2" />
+                </div>
+            </Slider>
         )
     }
 
